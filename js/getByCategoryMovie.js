1e20597d81538e655a94e2b33f6dc8accb9d7743
jakub41 <lemiszewski@gmx.com>
2019-11-16 15:06:26 +0100

    Front office changed getByCategory to show movies better

--- js/getByCategoryMovie.js
+++ js/getByCategoryMovie.js
@@ -1,70 +1,45 @@
-getCategoryAction = async () => {
-  const actionMovies = await Fetch.get("/movies/Action");
-  console.log(actionMovies);
-  showActionMovies(actionMovies);
-  return actionMovies;
+const getAllMovies = async () => {
+  const categories = await getCategories();
+  showSpinner();
+  categories.forEach(async category => {
+    const movies = await getmovies(category);
+    await showMovies(movies, category);
+  });
 };
 
-showActionMovies = actionMovies => {
+showMovies = async (movies, category) => {
   //action
-  let actionMoviesSection = H.$1("#action");
-  let movieRow = H.createNode("div");
-  movieRow.className = "row py-4";
+  let moviesSection = H.$1("#renderMovies");
+  let categoryHeading = H.createNode("h1");
+  categoryHeading.textContent = category;
+  moviesSection.appendChild(categoryHeading);
+  let sectionRender = H.createNode("section");
+  let card = moviesSection.appendChild(sectionRender);
+  let render = H.createNode("div");
+  render.className = "row py-4";
+  let movieDiv = card.appendChild(render);
 
-  let movieDiv = actionMoviesSection.appendChild(movieRow);
-
-  if(actionMovies.length > 0) {
-    movieDiv.innerHTML = actionMovies.map(
-        movie => `
-        <div class="col-md-3 py-2">
-            <div class="card bg-dark text-white bg-dark h-100 d-flex flex-column" id="${movie._id}">
-                <img class="card-img-top" src="${movie.imageUrl}" alt="product">
-                <div class="card-block">
-                <h4 class="card-title">${movie.name}</h4>
-                <p class="card-text">${movie.description}</p>
-                <a href="/details" class="btn btn-outline-light btn-sm">Details</a>
-                </div>
-            </div>
-        </div>   
-        `
-    ).join("");
+  if (movies.length > 0) {
+    await showAllMovies(movies, movieDiv);
   }
-
+};
+showAllMovies = async (allMovies, movieCard) => {
+  movieCard.innerHTML = allMovies
+    .map(
+      movie => `
+        <div class="col-md-3 py-2">
+        <div class="card bg-dark text-white bg-dark h-100 d-flex flex-column" id="${movie._id}">
+            <img class="card-img-top" src="${movie.imageUrl}" alt="product">
+            <div class="card-block">
+            <h4 class="card-title">${movie.name}</h4>
+            <p class="card-text">${movie.description}</p>
+            <a href="/details" class="btn btn-outline-light btn-sm">Details</a>
+            </div>
+        </div>
+    </div>   
+  `
+    )
+    .join("");
 };
 
-getCategoryScifi = async () => {
-    const scifi = await Fetch.get("/movies/Sci-Fi");
-    console.log(scifi);
-    showScifiMovies(scifi);
-    return scifi;
-  };
-  
-  showScifiMovies = scifi => {
-    //action
-    let scifiMoviesSection = H.$1("#scifi");
-    let movieRow = H.createNode("div");
-    movieRow.className = "row py-4";
-  
-    let movieDiv = scifiMoviesSection.appendChild(movieRow);
-  
-    if(scifi.length > 0) {
-      movieDiv.innerHTML = scifi.map(
-          movie => `
-          <div class="col-md-3 py-2">
-              <div class="card bg-dark text-white bg-dark h-100 d-flex flex-column" id="${movie._id}">
-                  <img class="card-img-top" src="${movie.imageUrl}" alt="product">
-                  <div class="card-block">
-                  <h4 class="card-title">${movie.name}</h4>
-                  <p class="card-text">${movie.description}</p>
-                  <a href="/details" class="btn btn-outline-light btn-sm">Details</a>
-                  </div>
-              </div>
-          </div>   
-          `
-      ).join("");
-    }
-  
-  };
-
-getCategoryScifi();
-getCategoryAction();
+getAllMovies();
