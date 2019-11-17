6184aab27fbe7d585a8b88a99c7b76b9cd77ed05
jakub41 <lemiszewski@gmx.com>
2019-11-17 13:11:32 +0100

    Adding a new movie working

--- js/movieAdd.js
+++ js/movieAdd.js
@@ -20,9 +20,11 @@ H.$1("#addMoviesForm>.btn").addEventListener("click", async e => {
   const category = H.$1("#category").value;
   const description = H.$1("#description").value;
   const image = H.$1("#image").value;
-  // We call the function to POST the data
-  if (await validateform(name, description, category, image)) {
-    if (id == "" && id == null) {
+
+  // We call the function to POST the data after the validation
+  // We add a new movie or we update a movie
+  if (await validateForm(name, description, category, image)) {
+    if (id == "" || id == null || id == undefined || isNaN(id)) {
       await addMoviesForm(name, description, category, image);
     } else {
       await updateMoviesForm(id, name, description, category, image);
@@ -40,9 +42,8 @@ addMoviesForm = async (name, description, category, image) => {
     description: description,
     imageUrl: image
   });
-
   //Error;
-  if (response !== 200) {
+  if (response.status && response.status !== 200) {
     H.$1("#addMoviesForm").reset();
     throw new Error("HTTP status " + response.status);
   }
